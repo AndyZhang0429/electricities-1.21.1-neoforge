5e59ce7c325a715a1cef266374be073ce03b3836
Andy Zhang <andyfushan@126.com>
2026-03-12 17:45:57 +0800

    Start to use BlockEntities

--- src/main/java/com/fengming/electricities/ponder/ModPonderPlugin.java
+++ src/main/java/com/fengming/electricities/ponder/ModPonderPlugin.java
@@ -1,11 +1,16 @@
 package com.fengming.electricities.ponder;
 
 import com.fengming.electricities.Electricities;
+import com.fengming.electricities.item.ModItems;
+
 import net.createmod.ponder.api.registration.PonderPlugin;
 import net.createmod.ponder.api.registration.PonderSceneRegistrationHelper;
+import net.createmod.ponder.api.registration.PonderTagRegistrationHelper;
 import net.minecraft.resources.ResourceLocation;
 
 public class ModPonderPlugin implements PonderPlugin {
+    final ResourceLocation LOCATION_BATTERY = ResourceLocation.fromNamespaceAndPath(Electricities.MODID,"battery");
+
     @Override
     public String getModId(){
         return Electricities.MODID;
@@ -13,8 +18,17 @@ public class ModPonderPlugin implements PonderPlugin {
 
     @Override
     public void registerScenes(PonderSceneRegistrationHelper<ResourceLocation> helper) {
-        helper.forComponents(ResourceLocation.fromNamespaceAndPath(Electricities.MODID,"battery"))
+        helper.forComponents(LOCATION_BATTERY)
                 .addStoryBoard("battery/battery_simple", ModBatteryPonders::PonderBatterySimple);
     }
 
+    @Override
+    public void registerTags(PonderTagRegistrationHelper<ResourceLocation> helper) {
+        ResourceLocation TagBasic = ResourceLocation.fromNamespaceAndPath(Electricities.MODID,"basic");
+        helper.registerTag(TagBasic)
+                .addToIndex()
+                .item(ModItems.BATTERY_ITEM.get())
+                .register();
+        helper.addTagToComponent(LOCATION_BATTERY, TagBasic);
+    }
 }
